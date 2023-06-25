26884118abcdf6c0ae9221b55a1260a88d6ea9c8
Quadragon <pebblemaker@byakuren.me>
2023-06-24 23:09:14 -0500

    Amend assignment 07

--- hw07/JoinList.hs
+++ hw07/JoinList.hs
@@ -1,5 +1,8 @@
 module JoinList where
 
+import Distribution.Simple.Utils (xargs)
+import Sized
+
 data JoinList m a
   = Empty
   | Single m a
@@ -7,11 +10,23 @@ data JoinList m a
   deriving (Eq, Show)
 
 (+++) :: (Monoid m) => JoinList m a -> JoinList m a -> JoinList m a
-Empty +++ x = x
-x +++ Empty = x
 lhs +++ rhs = Append (tag lhs <> tag rhs) lhs rhs
 
 tag :: (Monoid m) => JoinList m a -> m
-tag Empty = mempty
-tag (Single m _) = m
-tag (Append m _ _) = m
+tag (Single t _) = t
+tag (Append t _ _) = t
+tag _ = mempty
+
+indexJ ::
+  (Sized b, Monoid b) =>
+  Int ->
+  JoinList b a ->
+  Maybe a
+indexJ 0 (Single _ a) = Just a
+indexJ idx (Append q lhs rhs)
+  | idx < sizeL = indexJ idx lhs
+  | idx <= sizeC = indexJ (idx - sizeL) rhs
+ where
+  sizeL = getSize . size $ tag lhs
+  sizeC = getSize . size $ q
+indexJ _ _ = Nothing
